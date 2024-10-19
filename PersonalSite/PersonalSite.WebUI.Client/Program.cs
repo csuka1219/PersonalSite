d8aa75b0fa391aae60088af711c8274248be66b6
csuka1219 <csukabalazs1219@gmail.com>
2024-10-19 19:58:09 +0200

    add radzen.blazor

--- PersonalSite/PersonalSite.WebUI.Client/Program.cs
+++ PersonalSite/PersonalSite.WebUI.Client/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 namespace PersonalSite.WebUI.Client
 {
@@ -10,6 +11,8 @@ namespace PersonalSite.WebUI.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddRadzenComponents();
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
